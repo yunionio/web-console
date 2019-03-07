aa7f1e2bfff1e4364c5b1e8709b0677325488c6d
houjiazong <houjiazong@gmail.com>
2019-03-07 19:20:39 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/attribute-hyphenation': [2, 'always'],
     'vue/html-closing-bracket-newline': ['error', { 'multiline': 'never' }],
